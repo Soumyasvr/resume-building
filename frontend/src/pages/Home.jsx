0bf45f5c78678cbdd2d4b83a1f7bc692d833960b
soumyasvr <soumya.bsvr@gmail.com>
2026-01-23 20:10:23 +0530

    hero section completed

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/home/banner'
+import Hero from '../components/home/Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
