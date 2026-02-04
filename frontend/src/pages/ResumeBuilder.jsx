84bd3b96d263fdf89575dc4d3840c977efebd954
soumyasvr <soumya.bsvr@gmail.com>
2026-02-04 17:33:07 +0530

    Add PersonalInfoForm component and enhance ResumeBuilder with section navigation

--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom';
 import { dummyResumeData } from '../assets/assets';
-import { ArrowLeftIcon } from 'lucide-react';
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkle, User } from 'lucide-react';
+import PersonalInfoForm from '../components/PersonalInfoForm';
 
 const ResumeBuilder = () => {
 
@@ -30,6 +31,20 @@ const ResumeBuilder = () => {
     }
   }
 
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+
+  const sections = [
+    {id: "personal", name: "Personal Info", icon: User},
+    {id: "summary", name: "Summary", icon: FileText},
+    {id: "experience", name: "Experience", icon: Briefcase},
+    {id: "education", name: "Education", icon: GraduationCap},
+    {id: "skills", name: "Skills", icon: Sparkle},
+    {id: "projects", name: "Projects", icon: FolderIcon},
+  ]
+
+  const activeSection = sections[activeSectionIndex];
+
   useEffect(() => {
     loadExistingResume()
   },[])
@@ -41,6 +56,55 @@ const ResumeBuilder = () => {
         <ArrowLeftIcon className='size-4'/> Back to Dashboard
        </Link>
       </div>
+      <div  className='max-w-7xl mx-auto px-4 pb-8'>
+        <div className='grid lg:grid-cols-12 gap-8'>
+          {/* left panel -forms */}
+          <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+            <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
+              {/* progress bar using activeSectionIndex */}
+              <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200'/>
+              <hr style={{width:`${activeSectionIndex*100 / (sections.length -1)}%`}} className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to green-600 border-none transition-all duration-2000'/>
+              
+              {/* Section Naviagtion */}
+                <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+                  <div></div>
+                  <div className='flex items-center'>
+                    {activeSectionIndex !== 0 &&(
+                      <button className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 
+                      transaition-all' disabled={activeSectionIndex === 0} onClick={() => setActiveSectionIndex((prev) => Math.max(prev -1, 0))}>
+                        <ChevronLeft className='size-4'/> Previous
+                      </button>
+                    )}
+
+                    <button className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 
+                      hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={activeSectionIndex === sections.length - 1} onClick={() => setActiveSectionIndex((prev) => Math.min(prev + 1, sections.length - 1))}>
+                        Next <ChevronRight className='size-4'/>
+                      </button>
+
+                  </div>
+
+                </div>
+                {/* Form content */}
+                <div className='space-y-6'>
+                  {activeSection.id === "personal" && (
+                    <PersonalInfoForm 
+                    data={resumeData.personal_info} 
+                    onChange={(data) => setResumeData(prev => ({...prev, personal_info: data}))} 
+                    removeBackground={removeBackground} 
+                    setRemoveBackground={setRemoveBackground}/>
+                  )}
+
+                </div>
+            </div>
+          </div>
+          {/* Right panel - preview */}
+          <div>
+
+
+        </div>
+      </div>
+
+    </div>
     </div>
   )
 }
